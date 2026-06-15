306189721e09f06601e88b130cd5b990eca87048
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-06-15 18:19:40 +0400

    Add BiConditional Expression

---
+++ Logic/Expressions/BiConditional.cs
@@ -0,0 +1,17 @@
+﻿namespace WumpusWorld.Logic.Expressions {
+    public class BiConditional : IExpression {
+        public IExpression Left { get; }
+        public IExpression Right { get; }
+
+        public BiConditional(IExpression left, IExpression right) {
+            Left = left;
+            Right = right;
+        }
+
+        public bool Evaluate(Dictionary<string, bool> model) {
+            return Left.Evaluate(model) == Right.Evaluate(model);
+        }
+
+        public override string ToString() => $"{Left} ↔ {Right}";
+    }
+}
