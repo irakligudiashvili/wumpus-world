3ebaf5c643cebfc3971dd84d05e0f7135705e3d9
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-06-15 18:24:13 +0400

    Add inference classes

---
+++ Logic/Inference/Clause.cs
@@ -0,0 +1,60 @@
+﻿namespace WumpusWorld.Logic.Inference {
+    public class Clause {
+        public HashSet<Literal> Literals { get; } = new HashSet<Literal>();
+
+        public Clause() { }
+
+        public Clause(IEnumerable<Literal> literals) {
+            foreach(var l in literals) {
+                Literals.Add(l);
+            }
+        }
+
+        public bool IsEmpty => Literals.Count == 0;
+
+        public bool Equals(Clause? other) {
+            if(other == null) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, other)) {
+                return true;
+            }
+
+            if(Literals.Count != other.Literals.Count) {
+                return false;
+            }
+
+            return Literals.SetEquals(other.Literals);
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as Clause);
+
+        public override int GetHashCode() {
+            var hash = new HashCode();
+
+            var sorted = Literals
+                .OrderBy(l => l.SymbolName)
+                .ThenBy(l => l.IsPositive);
+
+            foreach (var lit in sorted) {
+                hash.Add(lit);
+            }
+
+            return hash.ToHashCode();
+        }
+
+        public override string ToString() {
+            if(Literals.Count == 0) {
+                return "Empty";
+            }
+
+            var sortedLiterals = Literals
+                .OrderBy(l => l.SymbolName)
+                .ThenBy(l => l.IsPositive)
+                .Select(l => l.ToString());
+
+            return string.Join(" ∨ ", sortedLiterals);
+        }
+    }
+}
---
+++ Logic/Inference/Literal.cs
@@ -0,0 +1,35 @@
+﻿namespace WumpusWorld.Logic.Inference {
+    public class Literal {
+        public string SymbolName { get; }
+        public bool IsPositive { get; }
+
+        public Literal(string symbolName, bool isPositive) {
+            SymbolName = symbolName;
+            IsPositive = isPositive;
+        }
+
+        public Literal Negate() => new Literal(SymbolName, !IsPositive);
+
+        public bool Equals(Literal? other) {
+            if (other == null) {
+                return false;
+            }
+
+            return SymbolName == other.SymbolName && IsPositive == other.IsPositive;
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as Literal);
+
+        public override int GetHashCode() {
+            return HashCode.Combine(SymbolName, IsPositive);
+        }
+
+        public override string ToString() {
+            if (!IsPositive) {
+                return $"¬{SymbolName}";
+            }
+
+            return SymbolName;
+        }
+    }
+}
