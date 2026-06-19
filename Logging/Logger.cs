8fd8270872d02dffadbe69ce1cb4ef27fdd42aea
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-06-19 21:27:03 +0400

    Creater logger

---
+++ Logging/Logger.cs
@@ -0,0 +1,13 @@
+﻿namespace WumpusWorld.Logging {
+    public class Logger {
+        public static List<string> Logs { get; private set; } = new List<string>();
+
+        public static void Log(string message) {
+            Logs.Add(message);
+        }
+
+        public static void Clear() {
+            Logs.Clear();
+        }
+    }
+}
