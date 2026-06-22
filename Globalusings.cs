ab4f1c391a9e9dd094cbbca812f57e1db7f8299a
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-06-22 21:13:28 +0400

    Add global usings

---
+++ Globalusings.cs
@@ -0,0 +1,8 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using WumpusWorld.Logging;
+global using WumpusWorld.Logic.Agent.Data;
+global using WumpusWorld.Logic.Agent.Controller;
+global using WumpusWorld.Logic.Expressions;
