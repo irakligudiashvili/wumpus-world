7d67143272db6bfcbc1837075caf1547aaa9880e
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-06-22 21:13:21 +0400

    Add KB logger

---
+++ Logging/KbLogger.cs
@@ -0,0 +1,14 @@
+﻿namespace WumpusWorld.Logging {
+    public static class KbLogger {
+        private static readonly List<string> _kbLogs = new List<string>();
+        public static IReadOnlyList<string> Logs => _kbLogs;
+
+        public static void Log(string message) {
+            _kbLogs.Add(message);
+        }
+
+        public static void Clear() {
+            _kbLogs.Clear();
+        }
+    }
+}
