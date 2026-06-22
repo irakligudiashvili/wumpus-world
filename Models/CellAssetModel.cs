8d0ec4be3010aceafb1a863d1f1d5f2e72937041
Irakli Gudiashvili <igudiashvili99@gmail.com>
2026-06-22 21:19:24 +0400

    Add models for view

---
+++ Models/CellAssetModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WumpusWorld.Models {
+    public class CellAssetModel {
+        public string? AgentImage { get; set; }
+        public bool HasGold { get; set; }
+        public bool HasWumpus { get; set; }
+        public bool HasPit { get; set; }
+        public bool HasStench { get; set; }
+        public bool HasBreeze { get; set; }
+
+        public bool IsEmpty => AgentImage == null && !HasGold && !HasWumpus && !HasPit && !HasStench && !HasBreeze;
+    }
+}
---
+++ Models/GameViewModel.cs
@@ -0,0 +1,46 @@
+﻿using WumpusWorld.Logic.Agent.Data;
+using WumpusWorld.Logic.Simulation;
+
+namespace WumpusWorld.Models {
+    public class GameViewModel {
+        public GameSimulation Simulation { get; set; }
+        public int GridSize => Simulation.Environment.Grid.Size;
+        public bool ShowSlowThinkingWarning { get; set; } = false;
+
+        public GameViewModel(GameSimulation simulation) {
+            Simulation = simulation;
+        }
+
+        public CellAssetModel GetCellAssets(int x, int y) {
+            var currentCell = new Coordinate(x, y);
+            var assets = new CellAssetModel();
+
+            // 1. Check Agent Status
+            if (Simulation.AgentState.Position.Equals(currentCell)) {
+                assets.AgentImage = Simulation.AgentState.IsAlive ? "agent.png" : "dead_agent.png";
+            }
+
+            // 2. Check Game Objects
+            if (Simulation.Environment.GoldPosition.Equals(currentCell) && !Simulation.AgentState.HasGold) {
+                assets.HasGold = true;
+            }
+            if (Simulation.Environment.IsWumpusAlive && Simulation.Environment.WumpusPosition.Equals(currentCell)) {
+                assets.HasWumpus = true;
+            }
+            if (Simulation.Environment.PitPositions.Contains(currentCell)) {
+                assets.HasPit = true;
+            }
+
+            // 3. Check Environmental Percepts
+            var cellPercept = Simulation.Environment.GetPerceptAt(currentCell, false);
+            if (cellPercept.Stench) {
+                assets.HasStench = true;
+            }
+            if (cellPercept.Breeze) {
+                assets.HasBreeze = true;
+            }
+
+            return assets;
+        }
+    }
+}
